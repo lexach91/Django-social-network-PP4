72f94cacdce96b76c676c68cfd3be83a49e3b038
lexach91 <ronmctailor@protonmail.com>
2022-03-17 19:10:46 +0800

    Add fields sent_on and updated_on to friend request model

--- friends/models.py
+++ friends/models.py
@@ -1,3 +1,4 @@
+from turtle import update
 from django.db import models
 from profiles.models import Profile
 
@@ -13,6 +14,9 @@ class FriendRequest(models.Model):
         on_delete=models.CASCADE,
         related_name='friend_request_to_profile'
     )
+    
+    sent_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
     
     accepted = models.BooleanField(default=False)
     declined = models.BooleanField(default=False)
