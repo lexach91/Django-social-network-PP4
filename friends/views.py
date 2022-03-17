2047d41dad3ed142066bc14fcc07a2bbb9dc7f6b
lexach91 <ronmctailor@protonmail.com>
2022-03-17 19:08:56 +0800

    Add ajax handlers to send and accept friend requests

--- friends/views.py
+++ friends/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import View
+from django.http import JsonResponse
+from .models import FriendRequest
+from profiles.models import Profile
 
 # Create your views here.
+class SendFriendRequest(View):
+    def post(self, request, *args, **kwargs):
+        if request.is_ajax():
+            from_profile = request.user.profile
+            to_profile = get_object_or_404(Profile, id=kwargs['id'])
+            friend_request = FriendRequest(
+                from_profile=from_profile,
+                to_profile=to_profile
+            )
+            friend_request.save()
+            return JsonResponse({'status': 'ok'})
+        return JsonResponse({'status': 'error'})
+
+
+class AcceptFriendRequest(View):
+    def post(self, request, *args, **kwargs):
+        if request.is_ajax():
+            friend_request = get_object_or_404(FriendRequest, id=kwargs['id'])
+            friend_request.accepted = True
+            accepting_profile = friend_request.to_profile
+            accepted_profile = friend_request.from_profile
+            accepting_profile.friends.add(accepted_profile)
+            accepted_profile.friends.add(accepting_profile)
+            accepting_profile.save()
+            accepted_profile.save()
+            friend_request.save()
+            return JsonResponse({'status': 'ok'})
+        return JsonResponse({'status': 'error'})
