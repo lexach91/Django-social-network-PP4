ee21a80f5d4916f32c740c1da6e2ad75a43cf11a
lexach91 <ronmctailor@protonmail.com>
2022-03-17 18:49:41 +0800

    Create model for friend requests

--- friends/models.py
+++ friends/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from profiles.models import Profile
 
 # Create your models here.
+class FriendRequest(models.Model):
+    from_profile = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        related_name='friend_request_from_profile'
+    )
+    to_profile = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        related_name='friend_request_to_profile'
+    )
+    
+    accepted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.from_profile} to {self.to_profile}'
--- social_network/settings.py
+++ social_network/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'home',
     'profiles',
+    'friends',
 ]
 
 MIDDLEWARE = [
