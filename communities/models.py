fdc1221d704c00f9c9c976544a7d288b5be2e117
lexach91 <ronmctailor@protonmail.com>
2022-03-21 16:11:58 +0800

    Add model for community

--- communities/models.py
+++ communities/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Community(models.Model):
+    name = models.CharField(max_length=100, unique=True, blank=False)
+    description = models.TextField(blank=True)
+    members = models.ManyToManyField(User, related_name='communities')
+    
+    def __str__(self):
+        return self.name
