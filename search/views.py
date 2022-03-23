437ccf3ebec49523529ad5618b4d5ed725868d20
lexach91 <ronmctailor@protonmail.com>
2022-03-23 15:14:19 +0800

    Create templates folder for search app

--- search/views.py
+++ search/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
+from profiles.models import Profile
+from communities.models import Community
 
 # Create your views here.
+class SearchView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'search/search.html')
