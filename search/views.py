25c4e42a8450b725bd41807a0f0508bcde4d9dba
lexach91 <ronmctailor@protonmail.com>
2022-03-23 17:12:25 +0800

    Add code to search profiles by username, first name, and last name

--- search/views.py
+++ search/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
 from django.views import View
+from django.http import HttpResponseRedirect
+from django.db.models import Q
 from profiles.models import Profile
 from communities.models import Community
 
@@ -7,3 +9,18 @@ from communities.models import Community
 class SearchView(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'search/search.html')
+    def post(self, request, *args, **kwargs):
+        search_type = request.POST.get('search_type')
+        search_query = request.POST.get('search_query')
+        if search_type == 'people':
+            # need to check query against username, first_name, last_name
+            profiles = Profile.objects.filter(
+                Q(user__username__icontains=search_query) |
+                Q(first_name__icontains=search_query) |
+                Q(last_name__icontains=search_query)
+            )
+            return render(request, 'search/search.html', {'search_results_people': profiles})
+        if search_type == 'communities':
+            communities = Community.objects.filter(name__icontains=search_query)
+            return render(request, 'search/search.html', {'communities': communities})
+        return HttpResponseRedirect(reverse('search'))
