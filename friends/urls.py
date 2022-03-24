f8fa369c075b8cfb7d52463ae884a71b98f85d90
lexach91 <ronmctailor@protonmail.com>
2022-03-24 16:48:24 +0800

    Add js code to send friend requests by ajax

--- friends/urls.py
+++ friends/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import SendFriendRequest, AcceptFriendRequest, DeclineFriendRequest
+from .views import SendFriendRequest, AcceptFriendRequest, DeclineFriendRequest, RemoveFriend
 
 
 urlpatterns = [
     path('send-friend-request/', SendFriendRequest.as_view(), name='send-friend-request'),
     path('accept-friend-request/', AcceptFriendRequest.as_view(), name='accept-friend-request'),
     path('decline-friend-request/', DeclineFriendRequest.as_view(), name='decline-friend-request'),
+    path('remove-friend/', RemoveFriend.as_view(), name='remove-friend'),
 ]
--- search/views.py
+++ search/views.py
@@ -13,18 +13,19 @@ class SearchView(View):
     def post(self, request, *args, **kwargs):
         search_type = request.POST.get('search_type')
         search_query = request.POST.get('search_query')
-        if search_type == 'people':
-            # need to check query against username, first_name, last_name
-            profiles = Profile.objects.filter(
-                Q(user__username__icontains=search_query) |
-                Q(first_name__icontains=search_query) |
-                Q(last_name__icontains=search_query) |
-                # if query is first and last name together, search for both
-                Q(first_name__icontains=search_query.split()[0], last_name__icontains=search_query.split()[-1]) |
-                Q(first_name__icontains=search_query.split()[-1], last_name__icontains=search_query.split()[0])
-            )
-            return render(request, 'search/search.html', {'search_results_people': profiles})
-        if search_type == 'communities':
-            communities = Community.objects.filter(name__icontains=search_query)
-            return render(request, 'search/search.html', {'communities': communities})
+        if search_type and search_query:
+            if search_type == 'people':
+                # need to check query against username, first_name, last_name
+                profiles = Profile.objects.filter(
+                    Q(user__username__icontains=search_query) |
+                    Q(first_name__icontains=search_query) |
+                    Q(last_name__icontains=search_query) |
+                    # if query is first and last name together, search for both
+                    Q(first_name__icontains=search_query.split()[0], last_name__icontains=search_query.split()[-1]) |
+                    Q(first_name__icontains=search_query.split()[-1], last_name__icontains=search_query.split()[0])
+                )
+                return render(request, 'search/search.html', {'search_results_people': profiles})
+            if search_type == 'communities':
+                communities = Community.objects.filter(name__icontains=search_query)
+                return render(request, 'search/search.html', {'communities': communities})
         return HttpResponseRedirect(reverse('search'))
