df7d5e53f1834a0e0e9b8e628b93dcb5bc3718cf
lexach91 <ronmctailor@protonmail.com>
2022-03-29 18:39:40 +0800

    Create model for posts

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,46 @@
+import profile
+from turtle import update
 from django.db import models
+from django.contrib.auth.models import User
+from matplotlib import image
+from numpy import imag
+from profiles.models import Profile
+from communities.models import Community
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+POST_TYPE_CHOICES = (
+    (1, 'profile_wall'),
+    (2, 'community_wall'),
+)
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        related_name='posts',
+    )
+    content = models.TextField(max_length=500)
+    image = CloudinaryField(
+        'post_image',
+        folder = 'posts',
+        null = True,
+        blank = True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    post_type = models.IntegerField(choices=POST_TYPE_CHOICES)
+    community = models.ForeignKey(
+        Community,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        null=True,
+        blank=True
+    )
+    profile = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        null=True,
+        blank=True
+    )
