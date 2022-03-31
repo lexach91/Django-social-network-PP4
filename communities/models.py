97211f3c6b8931cf242d996214d24926f2c5502a
lexach91 <ronmctailor@protonmail.com>
2022-03-31 19:02:21 +0800

    Add bg image and logo fields to community model

--- communities/models.py
+++ communities/models.py
@@ -1,11 +1,24 @@
 from turtle import update
 from django.db import models
+from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
 
 # Create your models here.
 class Community(models.Model):
     name = models.CharField(max_length=100, unique=True, blank=False)
     description = models.TextField(blank=True)
+    bg_image = CloudinaryField(
+        'community_bg_image',
+        folder = 'community_bg_images',
+        null = True,
+        blank = True
+    )
+    logo = CloudinaryField(
+        'community_logo',
+        folder = 'community_logos',
+        null = True,
+        blank = True
+    )  
     members = models.ManyToManyField(User, related_name='communities')
     creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_communities')
     created_on = models.DateTimeField(auto_now_add=True)
