9b34d37e55aa6e98246a15ed8648eb5211f4eb49
lexach91 <ronmctailor@protonmail.com>
2022-04-01 13:54:15 +0800

    Add static folder, base.css, and base.js

--- communities/models.py
+++ communities/models.py
@@ -18,7 +18,7 @@ class Community(models.Model):
         folder = 'community_logos',
         null = True,
         blank = True
-    )  
+    )
     members = models.ManyToManyField(User, related_name='communities')
     creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_communities')
     created_on = models.DateTimeField(auto_now_add=True)
