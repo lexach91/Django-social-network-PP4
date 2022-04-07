7cc5022795ea74c00d1bc3fa503d22caaee8adc0
lexach91 <ronmctailor@protonmail.com>
2022-04-07 19:26:44 +0800

    Add redirect to my profile when click on your username

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, redirect
 from django.views import View
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseRedirect
 from posts.forms import PostForm
 
 from .models import Profile
@@ -16,6 +16,8 @@ class MyProfileView(View):
 class UserProfileView(View):
     def get(self, request, username, *args, **kwargs):
         user_profile = Profile.objects.get(user__username=username)
+        if user_profile.user == request.user:
+            return HttpResponseRedirect(reverse('my_profile'))
         form = PostForm()
         posts = user_profile.posts.all()
         return render(request, 'profiles/user_profile.html', {'user_profile': user_profile, 'form': form, 'posts': posts})
