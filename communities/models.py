94e2221677222abdf104ba0dc32991bd9e275d7a
lexach91 <ronmctailor@protonmail.com>
2022-04-12 18:15:48 +0800

    Add basic structure to communities list view

--- communities/models.py
+++ communities/models.py
@@ -1,4 +1,3 @@
-from turtle import update
 from django.db import models
 from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
--- communities/urls.py
+++ communities/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import UsersCommunitiesView
+from .views import UsersCommunitiesView, CommunityView
 
 
 urlpatterns = [
     path('', UsersCommunitiesView.as_view(), name='users_communities'),
+    path('<slug>/', CommunityView.as_view(), name='community'),
 ]
     
