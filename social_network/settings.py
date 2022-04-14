5396021f77efb02cb7dc4978f3283aa0c5759eb8
lexach91 <ronmctailor@protonmail.com>
2022-04-14 13:02:42 +0800

    Add app 'messages'

--- social_network/settings.py
+++ social_network/settings.py
@@ -161,3 +161,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ASGI_APPLICATION = 'social_network.asgi.application'
