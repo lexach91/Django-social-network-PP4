a0a5c77d9b24ac18f29d0eec4829bb317309f0c3
lexach91 <ronmctailor@protonmail.com>
2022-04-14 15:13:26 +0800

    Add websocket url patterns in routing.py

---
+++ messages/routing.py
@@ -0,0 +1,8 @@
+from bottle_websocket import websocket
+from django.urls import re_path
+from .consumers import ChatConsumer
+
+
+websocket_urlpatterns = [
+    re_path(r'', ChatConsumer.as_asgi()),
+]
--- social_network/asgi.py
+++ social_network/asgi.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+import messages.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 # Initialize Django ASGI application early to ensure the AppRegistry
@@ -20,4 +22,9 @@ django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
     # Just HTTP for now. (We can add other protocols later.)
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            messages.routing.websocket_urlpatterns
+        )
+    )
 })
