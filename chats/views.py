e462587e86a90ca415c4a5f41f95b5eeee4c7381
lexach91 <ronmctailor@protonmail.com>
2022-04-16 14:08:04 +0800

    Add ChatView structure

--- chats/views.py
+++ chats/views.py
@@ -7,3 +7,12 @@ class MyMessagesView(View):
     def get(self, request, *args, **kwargs):
         chats = Chat.objects.filter(members=request.user)
         return render(request, 'chats/my_messages.html', {'chats': chats})
+    
+    
+class ChatView(View):
+    def get(self, request, *args, **kwargs):
+        chat = Chat.objects.get(id=kwargs['chat_id'])
+        # when the chat has been read, mark all messages as read
+        if not chat.messages.filter(is_read=False).exists():
+            chat.messages.update(is_read=True)
+        return render(request, 'chats/chat.html', {'chat': chat})
