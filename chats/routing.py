b3372177f2c0622b1c66f2978f5ffb470a52a08a
lexach91 <ronmctailor@protonmail.com>
2022-04-16 19:14:45 +0800

    Add simple structure to chat_detail template, create chatRoom.js

--- chats/routing.py
+++ chats/routing.py
@@ -3,5 +3,6 @@ from .consumers import ChatConsumer
 
 
 websocket_urlpatterns = [
-    re_path(r'', ChatConsumer.as_asgi()),
+    re_path(r'ws/chat-with-<str:username>', ChatConsumer.as_asgi()),
+    re_path(r'wss/chat-with-<str:username>/', ChatConsumer.as_asgi()),
 ]
