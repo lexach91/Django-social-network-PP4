30278a6203e41801cfe8f1e879832dd171e6d7e2
lexach91 <ronmctailor@protonmail.com>
2022-04-18 18:08:23 +0800

    Fix mistake in route in chats/routing.py

--- chats/routing.py
+++ chats/routing.py
@@ -3,6 +3,5 @@ from .consumers import ChatConsumer
 
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat-with-<str:username>', ChatConsumer.as_asgi()),
-    re_path(r'wss/chat-with-<str:username>/', ChatConsumer.as_asgi()),
+    re_path(r'^my_messages/chat-with-(?P<username>[\w.@+-]+)/$', ChatConsumer.as_asgi()),
 ]
