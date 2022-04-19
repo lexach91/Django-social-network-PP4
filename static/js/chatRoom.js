9c8913a195310032e9d9826e5f6fde105a938d12
lexach91 <ronmctailor@protonmail.com>
2022-04-19 17:40:26 +0800

    Add js code to append a new message to a chat

--- static/js/chatRoom.js
+++ static/js/chatRoom.js
@@ -10,12 +10,28 @@ $(document).ready(function() {
     socket.onmessage = function(event) {
         const data = JSON.parse(event.data);
         console.log(data);
-        if (data.type === 'chat_message') {
-            const chatMessage = $('<div class="chat-message"></div>');
-            const messageAuthor = $('<div class="message-author"></dive>');
-            
+        const chatMessage = $('<div class="chat-message"></div>');
+        const messageAuthor = $('<div class="message-author"></div>');
+        const messageText = $('<div class="message-text"></div>');
+        const messageTime = $('<div class="message-time"></div>');
+        let authorAvatar;
+        if (data.author.avatar) {
+            authorAvatar = $('<img class="author-avatar" src="' + data.author.avatar + '" />');
+        } else {
+            authorAvatar = $(
+              '<img class="author-avatar" src="/static/images/default_avatar.svg" />'
+            );
         }
-    }
+        messageAuthor.append(authorAvatar);
+        messageAuthor.append(data.author.username);
+        messageText.append(data.message.content);
+        messageTime.append(data.message.timestamp);
+        chatMessage.append(messageAuthor);
+        chatMessage.append(messageText);
+        chatMessage.append(messageTime);
+        messageContainer.append(chatMessage);
+        
+    };
 
     socket.onclose = function(event) {
         console.log('Socket is closed. Reconnect will be attempted in 1 second.', event.reason);
