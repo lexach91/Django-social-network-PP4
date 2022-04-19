5aba8c7ca563fba3c3602399d90fd6bdf2172878
lexach91 <ronmctailor@protonmail.com>
2022-04-19 17:38:42 +0800

    Add code to consumers.py to get data from db

--- chats/consumers.py
+++ chats/consumers.py
@@ -39,26 +39,24 @@ class ChatConsumer(AsyncWebsocketConsumer):
         print(username)
         print(chat_id)
         
-        await self.save_message(message, username, chat_id)
-                
-        # first_user = self.user
-        # second_user = User.objects.get(username=username)
-        # chat = Chat.objects.filter(members=first_user).filter(members=second_user).first()
-        # print(first_user, username)
-        # print(message)
-        # new_message = Message.objects.create(
-        #     chat=chat,
-        #     author=first_user,
-        #     content=message
-        # )
-        # new_message.save()
-        # Send message to room group
+        message = await self.save_message(message, username, chat_id)
+        print(message)
+        avatar_url = await self.get_avatar_url(username)
+        username = await self.get_profile_username(username)
+
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message,
-                'username': username,
+                'message': {
+                    'content': message.content,
+                    # timestamp should be in format like 'April 19, 2022, 7:54 a.m.'
+                    'timestamp': message.created_at.strftime('%B %d, %Y, %I:%M %p'),
+                    },
+                'author': {
+                    'username': username,
+                    'avatar': avatar_url,
+                    },
                 'chatId': chat_id
             }
         )
@@ -66,16 +64,16 @@ class ChatConsumer(AsyncWebsocketConsumer):
     # Receive message from room group
     async def chat_message(self, event):
         message = event['message']
-        username = event['username']
-        chat_id = event['chatId']
+        author = event['author']
+        # chat_id = event['chatId']
         print(message)
-        print(username)
-        print(chat_id)
+        print(author)
+        # print(chat_id)
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
             'message': message,
-            'username': username,
-            'chatId': chat_id
+            'author': author,
+            # 'chatId': chat_id
         }))
         
     # function to save message to database
@@ -93,3 +91,13 @@ class ChatConsumer(AsyncWebsocketConsumer):
         
         new_message.save()
         return new_message
+
+    @database_sync_to_async
+    def get_avatar_url(self, username):
+        user = User.objects.get(username=username)
+        return user.profile.avatar.url if user.profile.avatar else None
+    
+    @database_sync_to_async
+    def get_profile_username(self, username):
+        user = User.objects.get(username=username)
+        return str(user.profile)
