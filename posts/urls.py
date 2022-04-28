d9f9a79d3da2d09c327e7ddb5eaae8899c86420a
lexach91 <ronmctailor@protonmail.com>
2022-04-28 16:22:15 +0800

    Add url paths for liking/disliking posts

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import CreatePostAjaxView
+from .views import CreatePostAjaxView, LikePostAjaxView, DislikePostAjaxView
 
 
 urlpatterns = [
     path('create/', CreatePostAjaxView.as_view(), name='create_post_ajax'),
+    path('like/', LikePostAjaxView.as_view(), name='like_post_ajax'),
+    path('dislike/', DislikePostAjaxView.as_view(), name='dislike_post_ajax'),
 ]
