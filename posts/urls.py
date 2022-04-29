15213bae109912ee8bf539a37c0628e65103bd4b
lexach91 <ronmctailor@protonmail.com>
2022-04-29 16:02:26 +0800

    Add urls for create, like, and dislike comments

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
-from .views import CreatePostAjaxView, LikePostAjaxView, DislikePostAjaxView
+from .views import (
+    CreatePostAjaxView,
+    LikePostAjaxView,
+    DislikePostAjaxView,
+    CreateCommentAjaxView,
+    LikeCommentAjaxView,
+    DislikeCommentAjaxView
+)
 
 
 urlpatterns = [
-    path('create/', CreatePostAjaxView.as_view(), name='create_post_ajax'),
-    path('like/', LikePostAjaxView.as_view(), name='like_post_ajax'),
-    path('dislike/', DislikePostAjaxView.as_view(), name='dislike_post_ajax'),
+    path('create-post/', CreatePostAjaxView.as_view(), name='create_post_ajax'),
+    path('like-post/', LikePostAjaxView.as_view(), name='like_post_ajax'),
+    path('dislike-post/', DislikePostAjaxView.as_view(), name='dislike_post_ajax'),
+    path('create-comment/', CreateCommentAjaxView.as_view(), name='create_comment_ajax'),
+    path('like-comment/', LikeCommentAjaxView.as_view(), name='like_comment_ajax'),
+    path('dislike-comment/', DislikeCommentAjaxView.as_view(), name='dislike_comment_ajax'),
 ]
