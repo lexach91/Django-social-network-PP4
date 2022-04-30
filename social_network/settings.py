117226c97467d2b17ffd5a4f0fb010f55cf281b5
lexach91 <ronmctailor@protonmail.com>
2022-04-30 20:14:09 +0800

    Change allowed hosts variable in settings.py

--- social_network/settings.py
+++ social_network/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG") == "True"
 
-ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS"), 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['django-social-network-project.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
