acf4b2271180da69b76cdd805ed631b1132c1c6f
lexach91 <ronmctailor@protonmail.com>
2022-04-30 19:51:52 +0800

    Fix typo in asgi.py 'mysite'=>'social_network'

--- social_network/asgi.py
+++ social_network/asgi.py
@@ -14,7 +14,7 @@ from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 import chats.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_network.settings')
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
