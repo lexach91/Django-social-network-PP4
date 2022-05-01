3106c8d3cdfadc99b533bd4afe1756ebda44e629
lexach91 <ronmctailor@protonmail.com>
2022-05-01 15:29:39 +0800

    Add all cloudianry settings

--- social_network/settings.py
+++ social_network/settings.py
@@ -128,6 +128,13 @@ DATABASES = {
 }
 
 
+cloudinary.config(
+    cloud_name = os.environ.get("CLOUDINARY_CLOUD_NAME"),
+    api_key = os.environ.get("CLOUDINARY_API_KEY"),
+    api_secret = os.environ.get("CLOUDINARY_API_SECRET"),
+)
+
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
