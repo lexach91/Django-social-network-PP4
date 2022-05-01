62f884a61d6010b0871f3f27272134a0a716388c
lexach91 <ronmctailor@protonmail.com>
2022-05-01 15:57:38 +0800

    Add styles to post images

--- posts/forms.py
+++ posts/forms.py
@@ -5,10 +5,11 @@ from .models import Post, Comment
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['content', 'image', 'post_type', 'community', 'profile']
+        fields = ['content', 'image', 'video', 'post_type', 'community', 'profile']
         widgets = {
             'content': forms.Textarea(attrs={'rows': 3}),
             'image': forms.ClearableFileInput(attrs={'multiple': False}),
+            'video': forms.ClearableFileInput(attrs={'multiple': False}),
             'post_type': forms.HiddenInput(),
             'community': forms.HiddenInput(),
             'profile': forms.HiddenInput(),
