62eead695fa4c2404b63c80b6c936256c90ed0a0
lexach91 <ronmctailor@protonmail.com>
2022-05-01 14:40:48 +0800

    Change the order of imports in asgi.py

--- social_network/asgi.py
+++ social_network/asgi.py
@@ -8,16 +8,16 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_network.settings')
 
 from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chats.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_network.settings')
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
