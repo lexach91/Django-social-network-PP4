4ca108fdc3bff5e3d43dc9950312765bb059afc6
lexach91 <ronmctailor@protonmail.com>
2022-05-02 14:56:59 +0800

    Change routing.py urls for chat app

--- chats/routing.py
+++ chats/routing.py
@@ -3,5 +3,6 @@ from .consumers import ChatConsumer
 
 
 websocket_urlpatterns = [
-    re_path(r'^my_messages/chat-with-(?P<username>[\w.@+-]+)/$', ChatConsumer.as_asgi()),
+    re_path(r'ws/chat/(?P<room_name>\w+)/$', ChatConsumer.as_asgi()),
+    re_path(r'wss/chat/(?P<room_name>\w+)/$', ChatConsumer.as_asgi()),
 ]
