2a0e6d824d0553218b2da202728b48d8cafff838
lexach91 <ronmctailor@protonmail.com>
2022-05-03 15:55:14 +0800

    Create profiles/forms.py, add forms to change avatar and edit profile

---
+++ profiles/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from .models import Profile
+
+
+class ChangeAvatarForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['avatar']
+        widgets = {
+            'avatar': forms.ClearableFileInput(attrs={'multiple': False, 'class': 'edit-avatar-btn'}),
+        }
+        
+
+class EditProfileInfo(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['first_name', 'last_name', 'country', 'city', 'bio', 'birth_date']
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'country': forms.Select(attrs={'class': 'form-control'}),
+            'city': forms.Select(attrs={'class': 'form-control'}),
+            'bio': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+            'birth_date': forms.DateInput(attrs={'class': 'form-control'}),
+        }
