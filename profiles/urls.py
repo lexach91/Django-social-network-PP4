7a195bb9b1b5ce6f138161ea7c6bbd27c5383ea7
lexach91 <ronmctailor@protonmail.com>
2022-05-06 15:54:24 +0800

    Add edit and reset avatar buttons

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import MyProfileView, UserProfileView, EditAvatarAjaxView
+from .views import MyProfileView, UserProfileView, EditAvatarAjaxView, EditProfileView
 
 urlpatterns = [
     path('my_profile/', MyProfileView.as_view(), name='my_profile'),
     path('<str:username>/', UserProfileView.as_view(), name='user_profile'),
     path('my_profile/edit_avatar/', EditAvatarAjaxView.as_view(), name='edit_avatar_ajax'),
+    path('my_profile/edit/', EditProfileView.as_view(), name='edit_profile'),    
 ]
