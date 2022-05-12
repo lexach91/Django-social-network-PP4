429ccdaf19b49ca9dffdb656b8a5ff8af804a7bf
lexach91 <ronmctailor@protonmail.com>
2022-05-12 17:32:03 +0800

    Change urls for joining and leaving the community

--- communities/urls.py
+++ communities/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import UsersCommunitiesView, CommunityView
+from .views import UsersCommunitiesView, CommunityView, JoinCommunityView, LeaveCommunityView
 
 
 urlpatterns = [
     path('', UsersCommunitiesView.as_view(), name='users_communities'),
     path('<slug>/', CommunityView.as_view(), name='community'),
+    path('<slug>/join/', JoinCommunityView.as_view(), name='join_community'),
+    path('<slug>/leave/', LeaveCommunityView.as_view(), name='leave_community'),
 ]
     
