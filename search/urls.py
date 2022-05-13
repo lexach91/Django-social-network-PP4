548ae8e0e6fe4db857fd440048d18f995af53639
lexach91 <ronmctailor@protonmail.com>
2022-05-13 20:12:20 +0800

    Add urls to new views in search app

--- search/urls.py
+++ search/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import SearchView
+from .views import SearchView, SearchPeopleAjax, SearchCommunitiesAjax
 
 
 urlpatterns = [
     path('', SearchView.as_view(), name='search'),
+    path('people/', SearchPeopleAjax.as_view(), name='search_people_ajax'),
+    path('communities/', SearchCommunitiesAjax.as_view(), name='search_communities_ajax'),
 ]
