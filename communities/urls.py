44c54bc407f843156b083401fc1ef8ab660abbba
lexach91 <ronmctailor@protonmail.com>
2022-05-13 12:01:02 +0800

    Change the order of urls in communities app

--- communities/urls.py
+++ communities/urls.py
@@ -10,9 +10,9 @@ from .views import (
 
 urlpatterns = [
     path('', UsersCommunitiesView.as_view(), name='users_communities'),
+    path('create/', CreateCommunityView.as_view(), name='create_community'),
     path('<slug>/', CommunityView.as_view(), name='community'),
     path('<slug>/join/', JoinCommunityView.as_view(), name='join_community'),
     path('<slug>/leave/', LeaveCommunityView.as_view(), name='leave_community'),
-    path('create/', CreateCommunityView.as_view(), name='create_community'),
 ]
     
