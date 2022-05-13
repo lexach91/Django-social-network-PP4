faf065db07c3d9ca876ad885074d6b525d7c5dad
lexach91 <ronmctailor@protonmail.com>
2022-05-13 11:58:43 +0800

    Add communities/forms.py and create community form

---
+++ communities/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from .models import Community
+
+
+class CommunityForm(forms.ModelForm):
+    class Meta:
+        model = Community
+        fields = ['name', 'description', 'bg_image', 'logo']
+        
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'bg_image': forms.FileInput(attrs={
+                'class': 'form-control',
+                'accept': 'image/*'
+                }),
+            'logo': forms.FileInput(attrs={
+                'class': 'form-control',
+                'accept': 'image/*'
+                }),
+        }
