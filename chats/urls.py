40897a78ed543cdd6860c9aa6bf5c15fec2c1fdb
lexach91 <ronmctailor@protonmail.com>
2022-05-15 11:09:32 +0800

    Add url for GetMessageTimeView

--- chats/urls.py
+++ chats/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import MyMessagesView, ChatView
+from .views import MyMessagesView, ChatView, GetMessageTimeView
 
 
 urlpatterns = [
     path('', MyMessagesView.as_view(), name='my_messages'),
     path('chat-with-<str:username>/', ChatView.as_view(), name='chat_detail'),
+    path('get-message-time/', GetMessageTimeView.as_view(), name='get_message_time'),
 ]
