ee2256878bbf8d1edfef1be553d5e76080c320d4
lexach91 <ronmctailor@protonmail.com>
2022-05-15 20:23:45 +0800

    Add all event models to admin.py in feed app

--- feed/admin.py
+++ feed/admin.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
-from .models import PostEvent, CommentEvent, LikeDislikeEvent
+from .models import (
+    PostEvent,
+    CommentEvent,
+    LikeDislikeEvent,
+    FriendEvent,
+    FriendRequestDeclinedEvent,
+    FriendRequestEvent,
+    RemoveFriendEvent,
+    CommunityCreateEvent,
+    CommunityDeleteEvent,
+    CommunityJoinEvent,
+    CommunityLeaveEvent,
+)
 
 # Register your models here.
 admin.site.register(PostEvent)
 admin.site.register(CommentEvent)
 admin.site.register(LikeDislikeEvent)
+admin.site.register(FriendEvent)
+admin.site.register(FriendRequestDeclinedEvent)
+admin.site.register(FriendRequestEvent)
+admin.site.register(RemoveFriendEvent)
+admin.site.register(CommunityCreateEvent)
+admin.site.register(CommunityDeleteEvent)
+admin.site.register(CommunityJoinEvent)
+admin.site.register(CommunityLeaveEvent)
