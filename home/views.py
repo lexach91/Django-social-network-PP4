15593e893a5d3555adbe8e8cdfbb04a0d2ff47fb
lexach91 <ronmctailor@protonmail.com>
2022-05-16 21:11:30 +0800

    Add redirection to feed for registered users

--- home/views.py
+++ home/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 class HomeView(View):
     def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return HttpResponseRedirect('/feed/')
         return render(request, 'home/home.html', {})
