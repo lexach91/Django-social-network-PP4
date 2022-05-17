c1424d2988a1b05efb096f3676f86a6dca7e7a0d
lexach91 <ronmctailor@protonmail.com>
2022-05-17 19:35:07 +0800

    Add url for updating message status

--- chats/urls.py
+++ chats/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import MyMessagesView, ChatView, GetMessageTimeView
+from .views import MyMessagesView, ChatView, GetMessageTimeView, UpdateMessageReadStatusView
 
 
 urlpatterns = [
     path('', MyMessagesView.as_view(), name='my_messages'),
     path('chat-with-<str:username>/', ChatView.as_view(), name='chat_detail'),
     path('get-message-time/', GetMessageTimeView.as_view(), name='get_message_time'),
+    path('update-message-read-status/', UpdateMessageReadStatusView.as_view(), name='update_message_read_status'),
 ]
