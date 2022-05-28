63ac744ebccb1343b537a07d7ce349ac52a1ce2f
lexach91 <ronmctailor@protonmail.com>
2022-05-28 19:51:40 +0800

    Add ModelAdmin for post and comment models in posts app

--- posts/admin.py
+++ posts/admin.py
@@ -2,5 +2,31 @@ from django.contrib import admin
 from .models import Post, Comment
 # Register your models here.
 
-admin.site.register(Post)
-admin.site.register(Comment)
+# admin.site.register(Post)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'author',
+        'content',
+        'post_type',
+        'has_media',
+        'edited',
+        'created_at',
+        'updated_at'
+    )
+    list_filter = ['author', 'post_type', 'has_media', 'edited']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        'author',
+        'post',
+        'content',
+        'edited',
+        'created_at',
+        'updated_at'
+    )
+    list_filter = ['author', 'edited']
