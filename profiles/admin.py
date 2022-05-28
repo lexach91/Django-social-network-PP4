86a58dc3e62752857726656902b7653d5873459a
lexach91 <ronmctailor@protonmail.com>
2022-05-28 20:04:22 +0800

    Add ModelAdmin for Profile model in profiles app

--- profiles/admin.py
+++ profiles/admin.py
@@ -2,4 +2,21 @@ from django.contrib import admin
 from .models import Profile
 
 
-admin.site.register(Profile)
+# admin.site.register(Profile)
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'first_name',
+        'last_name',
+        'country',
+        'city',
+        'bio',
+        'friends_count',
+        'age',
+    )
+    list_filter = ['user', 'country', 'city']
+
+    def friends_count(self, obj):
+        return obj.friends.count()
+    friends_count.short_description = 'Friends count'
