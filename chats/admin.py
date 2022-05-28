bbd3c2eb1841107a05eb469793f298c0cd6b10bb
lexach91 <ronmctailor@protonmail.com>
2022-05-28 18:34:09 +0800

    Add ModelAdmin for Chat and Message models in chat app

--- chats/admin.py
+++ chats/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from .models import Chat, Message
 
-# Register your models here.
-admin.site.register(Chat)
-admin.site.register(Message)
+
+@admin.register(Chat)
+class ChatAdmin(admin.ModelAdmin):
+    """Chat admin"""
+    list_display = ('get_members', 'created_at',
+                    'last_message_at', 'unread_messages_count')
+
+    def get_members(self, obj):
+        return ", ".join([member.username for member in obj.members.all()])
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    """Message admin"""
+    list_display = ('chat', 'author', 'content',
+                    'is_read', 'created_at', 'updated_at')
+    list_filter = ('chat', 'author', 'is_read')
