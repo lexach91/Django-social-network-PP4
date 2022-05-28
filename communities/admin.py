4374d0efc323b4a13d5ec50ca79e6875e3b6e6dc
lexach91 <ronmctailor@protonmail.com>
2022-05-28 18:36:01 +0800

    Add ModelAdmin for Community model in communities app

--- communities/admin.py
+++ communities/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Community
-# Register your models here.
-admin.site.register(Community)
+
+
+@admin.register(Community)
+class CommunityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'description', 'creator', 'members_count')
+    list_filter = ('creator',)
+    search_fields = ('name', 'description')
+    prepopulated_fields = {'slug': ('name',)}
+
+    def members_count(self, obj):
+        return obj.members.count()
