887959f2edabb08ea39f943b050cfa8f46aaef02
lexach91 <ronmctailor@protonmail.com>
2022-05-29 12:40:44 +0800

    Fix too long lines in friends/urls.py

--- friends/urls.py
+++ friends/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
-from .views import SendFriendRequest, AcceptFriendRequest, DeclineFriendRequest, RemoveFriend, MyFriendsView, CancelFriendRequest
+from .views import (
+    SendFriendRequest,
+    AcceptFriendRequest,
+    DeclineFriendRequest,
+    RemoveFriend,
+    MyFriendsView,
+    CancelFriendRequest
+)
 
 
 urlpatterns = [
