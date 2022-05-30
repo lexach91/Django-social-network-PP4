358f700584582275ce2d2d5f5e5850ccec4b48c8
lexach91 <ronmctailor@protonmail.com>
2022-05-30 14:29:25 +0800

    Add docsting to home view in home app

--- home/views.py
+++ home/views.py
@@ -4,7 +4,10 @@ from django.http import HttpResponseRedirect
 
 
 class HomeView(View):
+    """Class based view for the home page"""
+
     def get(self, request, *args, **kwargs):
+        """GET method for the home page"""
         if request.user.is_authenticated:
             return HttpResponseRedirect('/feed/')
         return render(request, 'home/home.html', {})
